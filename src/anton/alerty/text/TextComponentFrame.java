31df49411a9203c3e3dc0c748e28daa31c9e2505
antsuprunyuk <antsuprunyuk@gmail.com>
2015-01-30 09:57:50 +0300

    29012015

--- src/anton/alerty/text/TextComponentFrame.java
+++ src/anton/alerty/text/TextComponentFrame.java
@@ -10,11 +10,45 @@ public class TextComponentFrame extends JFrame {
 	public static final int TEXTAREA_COLUMNS = 20;
 	
 	public TextComponentFrame() {
+		JPanel northPanel = new JPanel();
+		GridLayout grid = new GridLayout(2, 2);
+		northPanel.setLayout(grid);
 		
+		JTextField name = new JTextField();
+		JPasswordField password = new JPasswordField();
+		
+		northPanel.add(new JLabel("Your name: ", SwingConstants.RIGHT));
+		northPanel.add(name);
+		northPanel.add(new JLabel("Insert password: ", SwingConstants.RIGHT));
+		northPanel.add(password);
+		
+		add(northPanel, BorderLayout.NORTH);
+		
+		final JTextArea textArea = new JTextArea(TEXTAREA_ROWS, TEXTAREA_COLUMNS);
+		JScrollPane scrollPane = new JScrollPane(textArea);
+		
+		add(scrollPane, BorderLayout.CENTER);
+		
+		JPanel southPanel = new JPanel();
+		JButton button = new JButton("Insert");
+		southPanel.add(button);
+		add(southPanel, BorderLayout.SOUTH);
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				textArea.append("name: " + name.getText() + " password: " + new String(password.getPassword()) + "\n");
+				 
+			}
+		});
+		pack();
 	}
 
 	public static void main(String[] args) {
-		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				JFrame frame = new TextComponentFrame();
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+			}
+		});
 	}
-
 }
